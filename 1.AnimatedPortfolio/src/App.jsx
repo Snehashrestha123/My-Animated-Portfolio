d5aa0b35909579fec62f0178881517dbdd0de433
Snehashrestha123 <snehashrestha357@gmail.com>
2026-01-07 20:43:17 +0545

    Made nav bar

---
+++ 1.AnimatedPortfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./components/About/About"
+import Home from "./components/Home/home"
+import Nav from "./components/Nav/Nav"
+
+function App() {
+
+
+  return (
+    <>
+      <Nav/>
+      <Home/>
+      <About/>
+    </>
+  )
+}
+
+export default App
