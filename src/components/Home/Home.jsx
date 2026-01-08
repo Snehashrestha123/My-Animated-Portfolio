94192c92a6ff5c8b4d4b6c148cecc1f3e8831836
Snehashrestha123 <snehashrestha357@gmail.com>
2026-01-08 20:08:46 +0545

    Changes

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Home.css"
+function Home() {
+  return (
+    <div id="home">
+      <div className="lefthome">
+          
+         
+      </div>
+
+      <div className="righthome">
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
